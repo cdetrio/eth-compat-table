68bc1adb031a69f432705fd9985233982150f4ef
cdetrio <cdetrio@gmail.com>
2017-02-25 10:30:50 +0100

    add cpp-ethereum and pyethereum, with better column headings

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 [see here](http://cdetrio.github.io/eth-compat-table)
+
+### building:
+npm install
+cd node_modules/react-textfit && npm run build && cd ../..
+npm run start
--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import Collapse, { Panel } from 'rc-collapse';
+import { Textfit } from 'react-textfit';
+
 import _ from 'lodash';
 
 import './collapse.css';
@@ -10,29 +12,79 @@ import './App.css';
 let CLIENT_NAMES = [];
 let CLIENT_METAINFO = [];
 
+// github stats as of 2-24-2017
+const CLIENT_GITHUB_STARS = {
+  'cpp-ethereum': 838,
+  'go-ethereum': 3016,
+  'pyethereum': 698,
+  'parity': 801
+};
+
+const CLIENT_NAMES_SHORT = {
+  'cpp-ethereum': 'cpp-eth',
+  'go-ethereum': 'go-eth',
+  'pyethereum': 'pyeth',
+  'parity': 'parity'
+};
+
+
+const CLIENT_URLS = {
+  'cpp-ethereum:develop': 'https://github.com/ethereum/cpp-ethereum/tree/develop',
+  'go-ethereum:master': 'https://github.com/ethereum/go-ethereum/tree/master',
+  'go-ethereum:storage-at': 'https://github.com/fjl/go-ethereum/tree/storage-at',
+  'pyethereum:develop': 'https://github.com/ethereum/pyethereum/tree/develop',
+  'parity:master': 'https://github.com/ethcore/parity/tree/master'
+};
+
+
+function sortClientNames(rpc_test_data) {
+  let client_names = _.keys(rpc_test_data.simulations);
+
+  let sorted_client_names = _.sortBy(client_names, function (name) {
+    const name_canon = name.split(':')[0]; // clip name after colon
+    if (_.has(CLIENT_GITHUB_STARS, name_canon)) {
+      return -1 * CLIENT_GITHUB_STARS[name_canon];
+    } else {
+      console.log('couldnt find github stats for '+name_canon+' to sort by rank.');
+      return 0;
+    }
+  });
+
+  return sorted_client_names;
+}
+
+
 function processRpcTestData(rpc_test_data) {
   let client_testresults = [];
 
+  CLIENT_NAMES = sortClientNames(rpc_test_data);
+  console.log('CLIENT_NAMES:', CLIENT_NAMES);
+
+  CLIENT_METAINFO = new Array(CLIENT_NAMES.length);
+
   _.forEach(rpc_test_data.simulations, (val, key) => {
-    console.log("val['ethereum/rpeecee']:", val['ethereum/rpeecee']);
-    // start, end, success, subresults
+    // val['ethereum/rpeecee'] has keys: start, end, success, subresults
+    console.log(key, "val['ethereum/rpeecee']:", val['ethereum/rpeecee']);
+
+    const client_index = CLIENT_NAMES.indexOf(key);
 
     let meta_info = {
       'start': val['ethereum/rpeecee'].start,
       'end': val['ethereum/rpeecee'].end
     };
-    CLIENT_METAINFO.push(meta_info);
-    client_testresults.push(val['ethereum/rpeecee'].subresults);
-    CLIENT_NAMES.push(key);
+    CLIENT_METAINFO[client_index] = meta_info;
+
+    // TODO: should check if the subresults have the same length
+    // to ensure all clients have a result for all test cases
+
+    client_testresults[client_index] = val['ethereum/rpeecee'].subresults;
   });
 
-  console.log('CLIENT_NAMES:', CLIENT_NAMES);
 
   console.log('client_testresults:', client_testresults);
 
   let combined_results = _.zip(...client_testresults);
   //console.log('combined_results:', combined_results);
-
   // combined_results[0] = [
   //  {subresult.name, subresult.success, subresult.error, subresult.details},
   //  {subresult.name, subresult.success, subresult.error, subresult.details}
@@ -74,12 +126,14 @@ class ClientMetaInfo extends Component {
 
     return (
       <div>
-        {CLIENT_NAMES[clientIndex]}
+        <a href={CLIENT_URLS[CLIENT_NAMES[clientIndex]]}>
+          {CLIENT_NAMES[clientIndex]}
+        </a>
         <br/>
         <span className="testTimeInfo">
-          <a href={`${client_prefix}-client.log`}>client log</a>
+          <a href={`clientlogs/${client_prefix}-client.log`}>client log</a>
           &nbsp;&nbsp;
-          <a href={`${client_prefix}-simulator.log`}>test-runner log</a>
+          <a href={`clientlogs/${client_prefix}-simulator.log`}>test-runner log</a>
           <br/>
           start time: {CLIENT_METAINFO[clientIndex].start}
           <br/>
@@ -110,12 +164,33 @@ class TableHeader extends Component {
         if (client_name.includes('parity')) {
           logo_icon = 'logoIconParity';
         }
+
+        const clientName = client_name.split(':')[0];
+        const clientBranch = client_name.split(':')[1];
+
         return (
           <div className="header columnHeader" key={key_i}>
+
+            {/*
             <span className="clientColSpan">
-              {client_name}
+              {clientName}
             </span>
+            */}
+
+            <div className="clientNameContainer">
+              <Textfit mode="single" max={16}>
+                {CLIENT_NAMES_SHORT[clientName]}
+              </Textfit>
+              <br/>
+              <span className="clientBranch">
+                {clientBranch}
+              </span>
+
+            </div>
+
+            {/*
             <div className={logo_icon}></div>
+            */}
           </div>
         );
       });
@@ -149,7 +224,7 @@ class ClientTestResult extends Component {
     const pass_cell_html = (
       <div className={`testCell ${passOrFail(testObj.success)}`}>
         <div className={`${cssSuccess(testObj.success)}`}>
-          {testObj.success ? 'Yes' : 'No'}
+          {testObj.success ? '✓ Yes' : '✕ No'}
         </div>
       </div>
     );
@@ -251,7 +326,7 @@ function featureGroupComponent(groupTestCases, groupName, key_id, parent) {
   const testCount = groupTestCases.length;
 
   let test_item_i = 0;
-  let passing_counts = [0, 0, 0];
+  let passing_counts = new Array(CLIENT_NAMES.length).fill(0);
   const testItems = groupTestCases.map(test_obj => {
     //console.log('test case:', test_obj);
     test_item_i += 1;
@@ -262,6 +337,14 @@ function featureGroupComponent(groupTestCases, groupName, key_id, parent) {
       }
     });
 
+    let ClientTestResultsHtml =
+      CLIENT_NAMES.map( (client_name, client_index) => {
+        return (
+          <ClientTestResult clientIndex={client_index} key={client_index}
+                            testObj={test_obj[client_index]}
+                            testKey={test_item_i+'-'+client_index} />
+        );
+      });
 
     const test_itemrow_html = (
         <div className="testItem" key={test_item_i}>
@@ -271,9 +354,7 @@ function featureGroupComponent(groupTestCases, groupName, key_id, parent) {
             </span>
           </div>
 
-          <ClientTestResult clientIndex={0} testObj={test_obj[0]} testKey={test_item_i+'-0'} />
-          <ClientTestResult clientIndex={1} testObj={test_obj[1]} testKey={test_item_i+'-1'} />
-          <ClientTestResult clientIndex={2} testObj={test_obj[2]} testKey={test_item_i+'-2'} />
+          {ClientTestResultsHtml}
 
         </div>
     );
@@ -282,10 +363,19 @@ function featureGroupComponent(groupTestCases, groupName, key_id, parent) {
   });
 
 
-  let group_scores = [];
-  group_scores[0] = groupScore(passing_counts[0], testCount);
-  group_scores[1] = groupScore(passing_counts[1], testCount);
-  group_scores[2] = groupScore(passing_counts[2], testCount)
+  let group_scores = CLIENT_NAMES.map( (client_name, client_index) => {
+    return groupScore(passing_counts[client_index], testCount);
+  });
+
+
+  let ClientResultScores =
+    CLIENT_NAMES.map( (client_name, client_index) => {
+      return (
+        <div className={`titleCell ${group_scores[client_index]}`} key={client_index}>
+          {passing_counts[client_index]}/{testCount}
+        </div>
+      );
+    });
 
 
   const testTitle = (
@@ -299,9 +389,7 @@ function featureGroupComponent(groupTestCases, groupName, key_id, parent) {
           </div>
          </div>
 
-         <div className={`titleCell ${group_scores[0]}`}>{passing_counts[0]}/{testCount}</div>
-         <div className={`titleCell ${group_scores[1]}`}>{passing_counts[1]}/{testCount}</div>
-         <div className={`titleCell ${group_scores[2]}`}>{passing_counts[2]}/{testCount}</div>
+         {ClientResultScores}
      </div>
   );
 
@@ -333,15 +421,11 @@ class App extends Component {
   }
 
 
-
   onChangeItem(activeItemKey) {
-    console.log('change activeItemKey:', activeItemKey);
     this.setState({
       'activeItemKey': activeItemKey
     });
   }
-
-
 
 
   componentDidMount() {
@@ -369,6 +453,17 @@ class App extends Component {
       return featureGroupComponent(test_cases, group_name, test_counter, this);
     });
 
+    let ClientMetaInfos = CLIENT_NAMES.map( (client_name, client_index) => {
+      return (
+        <div key={client_index}>
+          <ClientMetaInfo clientIndex={client_index} />
+          <br/>
+        </div>
+      );
+    });
+
+    //let ClientMetaInfosHtml = ClientMetaInfos.join((<br/>));
+
 
     return (
       <div className="App">
@@ -383,16 +478,15 @@ class App extends Component {
 
           <div className="testMetaInfo">
             <h4>
-              Test harness: <a href="https://github.com/cdetrio/hive/tree/6d2d48f7f1e9ba19f9e8c17de735ecdb6e672edd/simulators/ethereum/rpeecee">Hive @6d2d48f</a>
+              {/* track version of Hive used
+              Test harness: <a href="https://github.com/karalabe/hive/tree/4c0f46b2da1b7db67d78e0c585cc845770314ff7">Hive @4c0f46b</a>
+              */}
+              Test harness: <a href="https://github.com/karalabe/hive/">Hive</a>
               <br/>
-              RPC test suite: <a href="https://github.com/cdetrio/interfaces/tree/master/rpc-specs-tests">v0.0.1</a>
+              RPC test suite: <a href="https://github.com/cdetrio/interfaces/tree/0fcb796440dea702e308710457346d29b051f365/rpc-specs-tests">v0.0.1</a>
             </h4>
 
-            <ClientMetaInfo clientIndex={0} />
-            <br/>
-            <ClientMetaInfo clientIndex={1} />
-            <br/>
-            <ClientMetaInfo clientIndex={2} />
+            {ClientMetaInfos}
           </div>
 
         </div>
